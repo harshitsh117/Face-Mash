92928f254e9be59f7454a8b50eb2b96bc952e734
harshitsh117 <harshish117@gmail.com>
2021-06-22 12:25:16 +0530

    Changes in images

--- static/js/index.js
+++ static/js/index.js
@@ -9,12 +9,12 @@ var rotate_close_button = document.getElementById("rotate-close-button")
 close_button_instructions.addEventListener("click", ()=>{
     console.log("close")
     instructions_container.classList.add("hide")
-    rotate_phone_container.classList.add("show")
+    // rotate_phone_container.classList.add("show")
 })
 
-rotate_close_button.addEventListener("click",()=>{
-    rotate_phone_container.classList.remove("show")
-})
+// rotate_close_button.addEventListener("click",()=>{
+//     rotate_phone_container.classList.remove("show")
+// })
 
 let arr = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"]
 var popup = document.getElementById("popup-container")
